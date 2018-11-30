71e31f77353633c533f6e4b63ebcc8ac14d05881
wesamdabit <40891252+wesamdabit@users.noreply.github.com>
2018-11-30 22:31:57 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -353,4 +353,22 @@ client.user.setGame(`2play | D3S Music`,"https://www.twitch.tv/wesamdabit")
 client.user.setStatus("dnd")
 });
 
+
+
+client.on('message', msg => {
+
+    if (msg.content == '2join') {
+        if (msg.member.voiceChannel) {
+
+     if (msg.member.voiceChannel.joinable) {
+         msg.member.voiceChannel.join().then(msg.react('white_check_mark'));
+     }
+    }
+}
+})
+client.on('ready', () => { //code bot not leave room voice //Bot Is Online
+    client.channels.get("507220444982804491").join(); //by : D3S Community
+    });
+
+
 client.login(process.env.BOT_TOKEN);
